c6af0462211a09e5c1eb1093d2ecf96655cf37a1
Dhondt Ryan <3625@holbertonschool.com>
2021-10-04 10:20:17 +0000

    sixth file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print text on demand
+ * Return: 0 if succeed,otherwise 1
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
