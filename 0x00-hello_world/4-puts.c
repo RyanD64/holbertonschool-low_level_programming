7e44a2c3995f635f0e1f2f584de5b07b82104e9f
Dhondt Ryan <3625@holbertonschool.com>
2021-10-04 09:12:02 +0000

    fifth file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(void)
+{
+char variable[] = "\"Programming is like building a multilingual puzzle";
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
