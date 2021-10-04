a481a2e0e0f820ad1543e7666d102db80a1368a2
Dhondt Ryan <3625@holbertonschool.com>
2021-10-04 09:45:13 +0000

    correction 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+* main - prints the phrase in the variable
+* Return: 0 if exists properly, non-zero otherwise
+*/
 int main(void)
 {
 char variable[] = "\"Programming is like building a multilingual puzzle";
