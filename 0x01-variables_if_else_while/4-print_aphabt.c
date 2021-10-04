f8f3c34e2cee2f30f7ac074d7162680d2f440d75
Dhondt Ryan <3625@holbertonschool.com>
2021-10-04 20:35:24 +0000

    fifth file

---
+++ 0x01-variables_if_else_while/4-print_aphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - print alphabet except q and e
+ * Return: 0 return always 0,otherwise 1
+ */
+
+int main(void)
+{
+int letter = 'a';
+
+while (letter <= 'z')
+while (letter != 'e' && 'p')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+
+}
