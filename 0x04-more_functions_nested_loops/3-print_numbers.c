2e5c1a94dee760feb0da94f8d04b64ad9bd2cde8
Dhondt Ryan <3625@holbertonschool.com>
2021-10-07 10:25:42 +0000

    readme

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * return: 0 otherwise 1
+ */
+
+void print_numbers(void)
+{
+int letter = '0'
+while (letter <= '9')
+{
+_putchar(letter);
+_putchar('\n');
+}
+
+return (0);
+}
