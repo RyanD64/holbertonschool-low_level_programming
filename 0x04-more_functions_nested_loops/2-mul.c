1714ad01a1e442e63afc64e705014fe29c7c18fc
Dhondt Ryan <3625@holbertonschool.com>
2021-10-07 09:16:50 +0000

    third file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * mul - multiplies two integers characters
+ * @a: print a random number
+ * @b: prints a random number
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
+
