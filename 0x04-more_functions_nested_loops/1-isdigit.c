d0d846413f1a38f47f516eaaf6ecd6cf5d1b78ad
Dhondt Ryan <3625@holbertonschool.com>
2021-10-07 08:53:22 +0000

    second file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _isdigit - tells if character is a digital number
+ * @c: prints a random character
+ * Return: 0 otherwise 1
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
