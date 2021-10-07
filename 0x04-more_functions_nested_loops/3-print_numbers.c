44ffe0e71072120b865c9c4ea448cd95ab84c33a
Dhondt Ryan <3625@holbertonschool.com>
2021-10-07 11:54:27 +0000

    fourth file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,15 @@
 
 /**
  * print_numbers - print numbers from 0 to 9
- * return: 0 otherwise 1
  */
 
 void print_numbers(void)
 {
-int letter = '0'
+int letter = '0';
 while (letter <= '9')
 {
 _putchar(letter);
-_putchar('\n');
+letter++;
 }
-
-return (0);
+_putchar('\n');
 }
