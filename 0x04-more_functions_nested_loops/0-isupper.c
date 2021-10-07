eed97a98ac806efd5d6acecaf3f20d3e22a60566
Dhondt Ryan <3625@holbertonschool.com>
2021-10-07 08:14:17 +0000

    first file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * _isupper - tell if character is upper
+ * @c: print a random character
+ * Return: 0 otherwise 1
+ */
+
+int _isupper(int c)
+{
+if ((c >= 65) && (c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+
+}
+}
+
