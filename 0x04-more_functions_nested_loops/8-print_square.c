344206d7aab3408822f3462ef5ade7a2ddc5116e
Dhondt Ryan <3625@holbertonschool.com>
2021-10-08 13:38:59 +0000

    ninth file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_square - print a diagonal with an n length
+ * @size: random number
+ */
+
+void print_square(int size)
+{
+	int ligne = 0;
+	int colonne = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (ligne = 0  ; ligne < size ; ligne++)
+		{
+			for (colonne = 0 ;colonne < size ; colonne++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
