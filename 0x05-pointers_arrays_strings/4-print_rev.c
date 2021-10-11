ed16019203e9e4c35de6baa4d868ff8624577418
Dhondt Ryan <3625@holbertonschool.com>
2021-10-11 13:43:05 +0000

    fifth file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include"main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: random character
+ */
+
+void print_rev(char *s)
+{
+int len = 0;
+int i;
+len = _strlen(s);
+
+for (i = len - 1; i >= 0; i--)
+{
+printf("%c", s[i]);
+}
+_putchar('\n');
+}
+
+
+/**
+ * _strlen - print the length of a string
+ * @s: random character
+ * Return: c otherwise 0
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+
+}
