56dc861fbea320cbf9212a8166c149f94e3e05c6
Dhondt Ryan <3625@holbertonschool.com>
2021-10-12 10:03:24 +0000

    seventh file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,21 @@
 #include"main.h"
 
-/*
- *
- *
+/**
+ * puts2 - puts half of the characters of a string
+ * @str: random number
  */
 
 void puts2(char *str)
 {
 int i;
-for(i = 0; i < 9; i = i + 2)
+for (i = 0; str[i] != '\0'; i = i + 2)
 {
-putchar(str[i]);
+	if (i % 2 == 0)
+	{
+		putchar(str[i]);
+	}
+	else
+		continue;
 }
 putchar('\n');
 }
