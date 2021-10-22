57cec607d2d682a6e48813ca353f993345a21c3a
RyanD64 <3625@holbertonschool.com>
2021-10-22 08:33:29 +0000

    first file

--- 0x0A-argc-argv/0-whatsmyname.c
+++ 0x0A-argc-argv/0-whatsmyname.c
@@ -2,13 +2,15 @@
 #include<stdlib.h>
 
 /**
-  *
-  *
+  * main - print the name of the file
+  * @argc: random value
+  * @argv: random character
+  * Return: 0 otherwise 1
   */
 
 int main(int argc, char *argv[])
 {
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 (void)argc;
-return 0;
+return (0);
 }
---
+++ 0x0A-argc-argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+  *
+  *
+  */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n",argc);
+(void)argv;
+return 0;:x
+
+}
