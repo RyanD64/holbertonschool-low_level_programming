bb47176788af0aef7902647b7e284856de21d580
RyanD64 <3625@holbertonschool.com>
2021-10-22 08:41:12 +0000

    second file

--- 0x0A-argc-argv/1-args.c
+++ 0x0A-argc-argv/1-args.c
@@ -2,14 +2,15 @@
 #include<stdlib.h>
 
 /**
-  *
-  *
+  * main - print the number of passed arguments
+  * @argc: random number
+  * @argv: random character
+  * Return: 0 otherwise 1
   */
 
 int main(int argc, char *argv[])
 {
-printf("%d\n",argc);
+printf("%d\n", argc);
 (void)argv;
-return 0;:x
-
+return (0);
 }
