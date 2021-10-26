89166aa8c3f7d4738610346328b03b47af8cef98
RyanD64 <3625@holbertonschool.com>
2021-10-26 09:02:08 +0000

    first file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,13 +4,18 @@
   * create_array - create an arrray of strings with malloc
   * @size: size of the variable
   * @c: random character
-  * Return: b otherwise 0
+  * Return: b otherwise NULL
   */
 
 char *create_array(unsigned int size, char c)
 {
 char *b = NULL;
 unsigned int n = 0;
+
+if (size == 0)
+{
+	return (NULL);
+}
 
 b = malloc((sizeof(unsigned int)) * (size));
 
