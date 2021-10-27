b27b384fcbaf4ccda83a500a4f8357a1018caa9b
RyanD64 <3625@holbertonschool.com>
2021-10-27 09:12:25 +0000

    second file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+  * *_strdup - print a copy of a string
+  * @str: random value
+  * Return: s otherwise NULL
+  */
+
+char *_strdup(char *str)
+{
+char *s = NULL;
+int f;
+s = malloc((sizeof(char)) * (_strlen(str) + 1));
+if (s == NULL)
+{
+return (NULL);
+}
+for (f = 0; str[f] != '\0'; f++)
+s[f] = str[f];
+
+return (s);
+}
+
+/**
+   * _strlen - print the length of a string
+    * @s: random value
+     * Return:c otherwise 0
+      */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
