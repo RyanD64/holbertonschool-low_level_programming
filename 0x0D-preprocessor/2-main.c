296ba935fe953bf021f0523f4614e5e6a17a0ee8
RyanD64 <3625@holbertonschool.com>
2021-11-02 10:33:18 +0000

    third file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
+/**
+  * main - print the name of the file before compilation
+  * Return: 0 otherwise 1
+  * @argc: random character
+  * @argv: random charcater
+  */
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	(void) argv;
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
