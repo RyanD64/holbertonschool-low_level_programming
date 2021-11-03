f9f1ece8b4241a5d1305058a12806a725e9631dc
RyanD64 <3625@holbertonschool.com>
2021-11-03 12:17:57 +0000

    third file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include"function_pointers.h"
+
+/**
+  * print_name - print the name of the user
+  * @name: random value
+  * @f: random function
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+	return;
+
+(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_POINTERS_H
+#define FUNCTIONS_POINTERS_H
+
+#include<stdio.h>
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+#endif
