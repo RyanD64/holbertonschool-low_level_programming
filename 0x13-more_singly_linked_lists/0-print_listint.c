8a01b0cdebf0638ed9aae77646b1fab4ccc5475e
RyanD64 <3625@holbertonschool.com>
2021-11-17 20:37:55 +0000

    first file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,25 +1,25 @@
 #include"lists.h"
 
 /**
-   *
-     *
-      *
-       *
-        * 
-	 */
+  *print_listint - print all the elements into a listint
+  *@h: random value
+  *Return: dumb otherwise 0
+  *
+  *
+*/
 
 size_t print_listint(const listint_t *h)
 {
 int dumb = 0;
 if (h == NULL)
 {
-	return(0);
+	return (0);
 }
-while(h != NULL)
+while (h != NULL)
 {
-printf("%d\n", h -> n);
+printf("%d\n", h->n);
 dumb++;
-h = h -> next;
+h = h->next;
 }
-return(dumb);
+return (dumb);
 }
