7864b0c34c9b087ddca7fb3520f0fc8f09745ce9
RyanD64 <3625@holbertonschool.com>
2021-11-17 22:15:35 +0000

    fourth file

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include"lists.h"
+
+/**
+  *
+  *
+  *
+  */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *begin;
+listint_t *temp;
+begin = malloc(sizeof(listint_t));
+
+if (begin == NULL)
+{
+	return (NULL);
+}
+begin->n = n;
+begin->next = NULL;
+
+if (*head == NULL)
+{
+	*head = begin;
+}
+else
+{
+temp = *head;
+	while (temp != NULL && temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+temp->next = begin;
+}
+return (*head);
+}
