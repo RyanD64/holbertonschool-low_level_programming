d21479a1f47fc1273a317dca945833fe913a613f
RyanD64 <3625@holbertonschool.com>
2021-11-19 08:34:27 +0000

    third file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,24 @@
 #include"main.h"
 
 /**
-  *
-  *
-  *
+  *get_bit - return the value of a byte at a given index
+  *@n: random number
+  *@index: random value
+  *Return: getbyte otherwise -1
   */
 
 int get_bit(unsigned long int n, unsigned int index)
+{
+int getbyte;
 
+if (index > 32 || n == 0)
+{
+	return (-1);
+}
+
+if (index <= 32)
+{
+	getbyte = (n >> index) & 1;
+}
+return (getbyte);
+}
