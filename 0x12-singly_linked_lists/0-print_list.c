0bef129163db83ea53ac23a31cb520c3dbfd0805
RyanD64 <3625@holbertonschool.com>
2021-11-19 14:19:33 +0000

    first file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include"lists.h"
+
+/**
+  *print_list - print all elements of a linked list
+  *@h: random value
+  *Return: i otherwise 0
+  */
+
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if ((h->str) == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+		}
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+studying single linked lists
