95a53eee170512652cf2acfff888a05aea11f18b
RyanD64 <3625@holbertonschool.com>
2021-11-19 09:01:01 +0000

    fourth file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,14 @@
 #include"main.h"
 
 /**
-  *get_bit - return the value of a byte at a given index
+  *set_bit - set the value of a bit to 1
   *@n: random number
   *@index: random value
   *Return: getbyte otherwise -1
   */
 
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-int getbyte;
-
 if (index > 32 || n == 0)
 {
 	return (-1);
@@ -18,7 +16,7 @@ if (index > 32 || n == 0)
 
 if (index <= 32)
 {
-	getbyte = (n >> index) & 1;
+	*n |= (1UL << index);
 }
-return (getbyte);
+return (1);
 }
