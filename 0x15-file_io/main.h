1c09f0cdfcec3ead68d2423881dcbe1f33d5f839
RyanD64 <3625@holbertonschool.com>
2021-11-22 17:35:02 +0000

    first file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,3 +11,4 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
+#endif
