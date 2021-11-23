7da2c19f57b62b99e9aac839eaa99ede19739e53
RyanD64 <3625@holbertonschool.com>
2021-11-23 19:29:01 +0000

    seventh file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _abs - print the absolute value of a number
+  *@b: random number
+  *Return: b
+  */
+
+int _abs(int b)
+{
+if (b < 0)
+{
+	b = b * (-1);
+}
+return (b);
+}
