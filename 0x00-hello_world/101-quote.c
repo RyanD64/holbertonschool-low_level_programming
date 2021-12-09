547660ee3658378fc8cfb77c5cbb1775089459a1
RyanD64 <3625@holbertonschool.com>
2021-12-09 23:07:55 +0000

    ninth file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Print a comment without usin prinf ans puts
+  * Return: 1 otherwise 0
+  */
+
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	while (*s)
+	{
+			putchar (*s++);
+	}
+	return (1);
+}
