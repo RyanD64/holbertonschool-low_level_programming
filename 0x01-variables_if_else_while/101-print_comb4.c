019678513bf5772d9e9f8496f58996d562512d3f
RyanD64 <3625@holbertonschool.com>
2021-12-10 09:19:30 +0000

    twelweth file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+  * main -  prints all possible different combinations of three digits.
+  * Return: 0 always
+  */
+
+int main(void)
+{
+int c = 48;
+int d = 49;
+int u = 50;
+
+while (c < 58)
+{
+	while (d < 58)
+	{
+		while (u < 58)
+		{
+			if (c == d || d == u || u == c)
+			{
+			}
+			else
+			{
+				putchar(c);
+				putchar(d);
+				putchar(u);
+				if (c == 55 && d == 56 && u == 57)
+				{
+				}
+				else
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		u++;
+		}
+	u = d + 1;
+	d++;
+	}
+d = c + 1;
+c++;
+}
+putchar('\n');
+return (0);
+}
