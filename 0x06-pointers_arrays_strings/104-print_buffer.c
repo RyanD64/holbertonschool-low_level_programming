de02cb651b47756f475b1cbdb87b15d6fa0fc494
RyanD64 <3625@holbertonschool.com>
2021-12-10 13:48:37 +0000

    thirteenth file

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,30 +13,30 @@ int i = 0, charactere;
 
 while (i < size)
 {
-if (i % 10 == 0)
-{
-printf("%08x: ", i);
-printf("%02x", b[i]);
-}
-i++;
-while (i % 10 != 0 && i < size)
-{
-if (i % 2 != 0)
-printf("%02x ", b[i]);
-else
-printf("%02x", b[i]);
-i++;
-}
-if (i % 10 != 0)
-print_last_line(b, i);
-for (charactere = i - 10; charactere < i && i % 10 == 0; charactere++)
-{
-if (b[charactere] > 31 && b[charactere] < 126)
-printf("%c", b[charactere]);
-else
-printf(".");
-}
-printf("\n");
+	if (i % 10 == 0)
+	{
+		printf("%08x: ", i);
+		printf("%02x", b[i]);
+	}
+	i++;
+	while (i % 10 != 0 && i < size)
+	{
+		if (i % 2 != 0)
+		printf("%02x ", b[i]);
+		else
+		printf("%02x", b[i]);
+		i++;
+	}
+	if (i % 10 != 0)
+		print_last_line(b, i);
+	for (charactere = i - 10; charactere < i && i % 10 == 0; charactere++)
+	{
+		if (b[charactere] > 31 && b[charactere] < 126)
+			printf("%c", b[charactere]);
+		else
+		printf(".");
+	}
+	printf("\n");
 }
 }
 
@@ -53,15 +53,17 @@ int j;
 
 for (j = i; j % 10 != 0; j++)
 {
-if (j % 2 == 0)
-printf(" ");
-printf("  ");
+	if (j % 2 == 0)
+	{
+		printf(" ");
+		printf("  ");
+	}
 }
 for (j = i - i % 10; j < i; j++)
 {
-if (b[j] > 10)
-printf("%c", b[j]);
-else
-printf(".");
+	if (b[j] > 10)
+		printf("%c", b[j]);
+	else
+		printf(".");
 }
 }
