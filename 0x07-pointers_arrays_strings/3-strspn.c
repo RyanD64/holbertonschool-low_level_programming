eda802e6fad3e820b36a89b9f225f95201e9107b
RyanD64 <3625@holbertonschool.com>
2021-12-10 14:44:07 +0000

    fourth file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,34 +1,39 @@
 #include"main.h"
 
 /**
-  *
-  *
-  *
+  * _strspn - function that gets the length of a prefix substring
+  * @s: string to be controlled
+  * @accept: charactere to control in s
+  * Return: number of bytes in the initial segment of s
   */
 
 unsigned int _strspn(char *s, char *accept)
 {
-int i;
-int j;
-int k = 0;
-int count = 0;
-for (i = 0; s[i] == '\0'; i++)
-{ 
-k = 0;
-	for (j = 0; accept[j] =='\0'; j++)
-	{
-		if (s[i] == accept[j])
-			{
-			count++;
-			k = 1;
-			}
-		else
-		break;
-	}
-if (k == 0)
+
+unsigned int len = 0;
+
+if ((*s == '\0') || (*accept == '\0'))
+return (len);
+
+while (*s && _strchr(accept, *s++))
 {
-return (count);
+len++;
 }
+return (len);
 }
-return (0);
+
+#include "main.h"
+
+/**
+  * _strchr - function that locates a character in a string
+  * @s: string to find
+  * @c: charactere to dearch
+  * Return: first occurence of c in s or 0 if not fund
+  */
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0' && *s != c)
+++s;
+return ((*s != '\0' || c == '\0') ? s : 0);
 }
