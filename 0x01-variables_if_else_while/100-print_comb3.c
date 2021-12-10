1a5dbf65f3ff2e80d1d9bbab8f82f5fd5579c6c3
RyanD64 <3625@holbertonschool.com>
2021-12-10 08:10:15 +0000

    eleventh file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  * main - prints all possible different combinations of two digits.
+  * Return: 0
+  */
+
+int main(void)
+{
+int i = 48;
+int c = 48;
+int cpt = 0;
+
+while (i < 58)
+{
+	while (c < 58)
+	{
+		if (c != i)
+		{
+			putchar(i);
+			putchar(c);
+			if (i < 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			c++;
+		}
+		else
+		c++;
+	}
+cpt = cpt + 1;
+c = 48 + cpt;
+i++;
+}
+putchar('\n');
+return (0);
+}
