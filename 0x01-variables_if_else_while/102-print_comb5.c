ae95f8e9b94236fb82a398657f488f9cf4ded4bc
RyanD64 <3625@holbertonschool.com>
2021-12-10 09:28:58 +0000

    thirtennth file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  * main - prints all possible different combinations of 3 digits.
+  * Return: 0
+  */
+
+int main(void)
+{
+int num1, num2;
+num1 = 0;
+num2 = num1 + 1;
+
+while (num1 <= 98)
+{
+	while (num2 <= 99)
+	{
+		putchar((num1 / 10) + '0');
+		putchar((num1 % 10) + '0');
+		putchar(32);
+		putchar((num2 / 10) + '0');
+		putchar((num2 % 10) + '0');
+		if (num1 == 98 && num2 == 99)
+			{
+			}
+		else
+		{
+		putchar(44);
+		putchar(32);
+	}
+	num2++;
+	}
+num2 = num1 + 2;
+num1++;
+}
+putchar('\n');
+return (0);
+}
