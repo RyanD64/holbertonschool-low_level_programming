1130963a6f54ac50a1af8b7de578444761a4600f
RyanD64 <3625@holbertonschool.com>
2021-12-10 11:30:46 +0000

    troubleshooting attempt

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,31 @@
 #include"main.h"
 
 /**
-    *
-      *
-        */
+  * *cap_string - capitalize all words of a string
+  * @s: random character
+  *Return: s otherwise 0
+  */
 
 char *cap_string(char *s)
 {
-	int a, b;
-	char separateur[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+int a, b;
+char c[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	a = 1;
-	while (s[a] != '\0')
-	{
-			if(a == 0 && s[a] >= 'a' && s[a] <= 'z')
-		{
-			s[a] = s[a] - 32;
-			}
-			for (b = 0; separateur[b] != '\0'; b++)
-						{
-			if ((s[a - 1] == separateur[b]) && (s[a] >= 'a' && s[a] <= 'z'))
-			{
-			s[a] =s[a] - 32;									
-			}
+a = 1;
+if (s[0] >= 97 && s[0] <= 122)
+{
+	s[0] = s[0] - 32;
 }
-	a++;
+while (s[a] != '\0')
+{
+for (b = 0; c[b] != '\0'; b++)
+{
+if (s[a - 1] == c[b] && (s[a] >= 97 && s[a] <= 122))
+{
+s[a] = s[a] - 32;
+}
+}
+a++;
 }
 return (s);
 }
