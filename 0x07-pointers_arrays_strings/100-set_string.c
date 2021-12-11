2ab7d6d9547fd4a2d58535058b046b297bccf9be
RyanD64 <3625@holbertonschool.com>
2021-12-11 08:59:13 +0000

    tenth file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *set_string - set the value of a pointer to a char
+  *@s: random character
+  *@to: random character
+  */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
