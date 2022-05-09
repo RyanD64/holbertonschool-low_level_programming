27ce2129b3cfc44e1420a94d97188c64e961b082
RyanD64 <3625@holbertonschool.com>
2022-05-09 14:28:57 +0000

    correction

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include"main.h"
+
+/**
+ * _isupper - tell if character is upper
+ * @c: print a random character
+ * Return: 0 otherwise 1
+ */
+
+int _isupper(int c)
+{
+if ((c >= 65) && (c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+
+}
+}
+
