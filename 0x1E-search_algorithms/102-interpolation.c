ff21ad940bbe33b6410b3be6ec162229503c676b
RyanD64 <3625@holbertonschool.com>
2022-08-31 06:56:39 +0000

    second advanced

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * jump_search - searches for a value using the jump search algorithm
+ * interpolation_search - search for a value using the interpol search algoritm
  * @array: array of integers
  * @size: size of the array
  * @value: value to search
@@ -10,5 +10,38 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
+	int l = 0;
+	int r = (size - 1);
+	size_t m;
 
+	if (array == NULL)
+		return (-1);
+
+	while (size)
+	{
+		m = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+		if (m >= size)
+		{
+			break;
+		}
+
+		printf("Value checked array[%ld] = [%d]\n", m, value);
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
+		else
+			return (m);
+	}
+
+	if (value == array[l])
+	{
+		printf("Found [%d] at index: [%d]\n", value, l);
+		return (l);
+	}
+	else
+	{
+		printf("Value checked array[%ld] is out of range\n", (size_t)m);
+		return (-1);
+	}
 }
