73a7207c3c9161efd20bf4abc1e762c45c8a2eef
RyanD64 <3625@holbertonschool.com>
2022-08-31 12:56:54 +0000

    test

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -25,7 +25,7 @@ int interpolation_search(int *array, size_t size, int value)
 			break;
 		}
 
-		printf("Value checked array[%ld] = [%d]\n", m, value);
+		printf("Value checked array[%ld] = [%d]\n", m, array[m]);
 		if (array[m] < value)
 			l = m + 1;
 		else if (array[m] > value)
